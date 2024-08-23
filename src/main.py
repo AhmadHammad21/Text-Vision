624d394ab46c291d6ff566aa2007fc8159cdcc3d
AhmadHammad21 <ahmedhammad8765@gmail.com>
2024-08-23 23:10:33 +0300

    Fix Relative Paths for Instance

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from rule_based_model import RuleBasedModel
 from format_results import FormatResults
 
 
-def generate_things():
+def generate_results():
     file_path = r'C:\Users\user\OneDrive\Noura_OCR\Invoices\Invoices\Cleaned_Invoices\pdf_text\6220327945_Gulf Medical Co Ltd _202401.pdf'
 
     # Process text extraction
