ba6b396e8d69322e6a189a2050775e40c9b826ae
Noura Abdelhafez <44889573+noura-na@users.noreply.github.com>
2024-08-24 21:36:29 +0300

    Update format_results.py

--- src/format_results.py
+++ src/format_results.py
@@ -1,13 +1,13 @@
 from logger import logging
 
-
 class FormatResults:
-    def __init__(self, llm_predictions: dict, rules_predictions: dict):
+    def __init__(self, llm_predictions: dict, rules_predictions: dict, payment_predictions: dict):
         """
-        Initialize the FormatResults class with two dictionaries:
-        llm_predictions and rules_predictions.
+        Initialize the FormatResults class with three dictionaries:
+        llm_predictions, payment_predictions, and rules_predictions.
         """
         self.llm_predictions = llm_predictions
+        self.payment_predictions = payment_predictions
         self.rules_predictions = rules_predictions
 
         # Priority values
@@ -16,7 +16,7 @@ class FormatResults:
 
     def process(self) -> dict:
         """
-        Processes the llm_predictions and rules_predictions dictionaries
+        Processes the llm_predictions, payment_predictions, and rules_predictions dictionaries
         to produce a final merged dictionary based on specific rules.
 
         Returns:
@@ -25,12 +25,15 @@ class FormatResults:
         logging.info('FormatResults.process Started')
         logging.info('LLM Predictions: ')
         logging.info(self.llm_predictions)
+        logging.info('Payment Predictions: ')
+        logging.info(self.payment_predictions)
         logging.info("Rules Predictions: ")
         logging.info(self.rules_predictions)
 
         # Initialize the final dictionary
         final_predictions = {}
 
+        # Merge LLM and Rules predictions with prioritization
         for key, llm_value in self.llm_predictions.items():
             rules_value = self.rules_predictions.get(key)
 
@@ -41,8 +44,11 @@ class FormatResults:
                 final_predictions[key] = rules_value
             else:
                 # Default to LLM value for other features
-                # NOTE: Re-Check
                 final_predictions[key] = llm_value
+
+        # Add Payment Method and Payment Terms predictions
+        final_predictions['Payment Method'] = self.payment_predictions.get('Payment Method', self.rules_predictions.get('Payment Method'))
+        final_predictions['Payment Terms'] = self.payment_predictions.get('Payment Terms', self.rules_predictions.get('Payment Terms'))
 
         logging.info("Final Predictions")
         logging.info(final_predictions)
@@ -50,4 +56,3 @@ class FormatResults:
         logging.info('FormatResults.process Finished')
 
         return final_predictions
-    
